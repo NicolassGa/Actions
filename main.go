efe866705de12b018fdb2514e713ac29c41d7d54
NicolassGa <ngallardoa@icloud.com>
2025-04-03 13:37:07 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func Add(a, b int) int {
 
-	return a + b
+	return a * b
 
 }
 
@@ -34,26 +34,3 @@ func additionHandler(w http.ResponseWriter, r *http.Request) {
 
 	b, err := strconv.Atoi(bStr)
 
-	if err != nil {
-
-		http.Error(w, "Parameter 'b' must be an integer", http.StatusBadRequest)
-
-		return
-
-	}
-
-	result := Add(a, b)
-
-	fmt.Fprintf(w, "%d", result)
-
-}
-
-func main() {
-
-	http.HandleFunc("/addition", additionHandler)
-
-	fmt.Println("Server listening on port 8080....")
-
-	http.ListenAndServe(":8080", nil)
-
-}
